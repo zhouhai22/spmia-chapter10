71e2368bb74244553a899187224800a2cefcfae9
zhouchunhai <zhouchunhai@cmss.chinamobile.com>
2021-04-25 10:04:58 +0800

    .travis.yml增加构建docker镜像并推送到dockerHub仓库

--- travis_scripts/deploy_to_docker_hub.sh
+++ travis_scripts/deploy_to_docker_hub.sh
@@ -1,8 +1,8 @@
 echo "Pushing service docker images to docker hub ...."
-docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-docker push zch22/tmx-authentication-service:$BUILD_NAME
-docker push zch22/tmx-licensing-service:$BUILD_NAME
-docker push zch22/tmx-organization-service:$BUILD_NAME
-docker push zch22/tmx-confsvr:$BUILD_NAME
-docker push zch22/tmx-eurekasvr:$BUILD_NAME
-docker push zch22/tmx-zuulsvr:$BUILD_NAME
+docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
+docker push zch22/tmx-authentication-service:"$BUILD_NAME"
+docker push zch22/tmx-licensing-service:"$BUILD_NAME"
+docker push zch22/tmx-organization-service:"$BUILD_NAME"
+docker push zch22/tmx-confsvr:"$BUILD_NAME"
+docker push zch22/tmx-eurekasvr:"$BUILD_NAME"
+docker push zch22/tmx-zuulsvr:"$BUILD_NAME"
